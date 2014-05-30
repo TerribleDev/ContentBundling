d855191879a1bae850b19a81dceab8f360359d23
Tommy Parnell <tparnell@vistaprint.com>
2014-05-30 01:39:33 -0400

    adding squishit project

---
+++ src/tparnell.ContentBundling.Squishit/BundlesFactory.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using EventManager.Web.Bundles;
+using SquishIt.Framework;
+using SquishIt.Framework.CSS;
+using tparnell.ContentBundling.Attributes;
+using tparnell.ContentBundling.Enum;
+
+namespace tparnell.ContentBundling.Squishit
+{
+    public sealed class BundlesFactory : IBundlesFactory
+    {
+        /// <summary>
+        /// Bundles Javascript
+        /// </summary>
+        /// <param name="bundle">Bundle Attribute</param>
+        /// <param name="debug">Debug Flag</param>
+        private static void BundleJs(IBundleAttribute bundle, bool debug)
+        {
+                BundlesProcessor.StandardProcess<SquishIt.Framework.JavaScript.JavaScriptBundle>( bundle, debug, fd =>
+                {
+                    //Add the defer or async attribute to html tags if the attribute wants to go async
+                    switch (bundle.Async)
+                    {
+                        case AsyncLoading.Defer:
+                            fd.WithAttribute("defer", string.Empty);
+                            break;
+                        case AsyncLoading.Default:
+                            fd.WithAttribute("defer", string.Empty);
+                            break;
+                        case AsyncLoading.Async:
+                            fd.WithAttribute("async", string.Empty);
+                            break;
+                    }
+                    return fd;
+                });
+                
+            
+        }
+        /// <summary>
+        /// Bundles CSS
+        /// </summary>
+        /// <param name="bundle">Bundle attribute</param>
+        /// <param name="debug">debug flag</param>
+        private static void BundleCss(IBundleAttribute bundle, bool debug)
+        {
+          BundlesProcessor.StandardProcess<CSSBundle>( bundle, debug, cssBundle => cssBundle);
+            
+        }
+
+        public void BundleContent(IBundleAttribute bundle, bool debug = false)
+        {
+            if (bundle == null) throw new ArgumentNullException("bundle");
+                if (bundle.Contenttype == ContentType.JavaScript) BundleJs(bundle, debug);
+                if (bundle.Contenttype == ContentType.Css) BundleCss(bundle, debug);
+            
+        }
+
+
+        public void RebuildCache()
+        {
+            Bundle.ClearPreprocessors();
+        }
+    }
+}
---
+++ src/tparnell.ContentBundling.Squishit/BundlesProcessor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using SquishIt.Framework;
+using SquishIt.Framework.Base;
+using tparnell.ContentBundling.Attributes;
+
+namespace tparnell.ContentBundling.Squishit
+{
+    internal sealed class BundlesProcessor
+    {
+        /// <summary>
+        /// This is where the bundling actually happens
+        /// </summary>
+        /// <typeparam name="T">T is the the type of squishit class to use </typeparam>
+        /// <param name="bundle">A bundle attribute</param>
+        /// <param name="debug">debug flag</param>
+        /// <param name="d">preprocessing</param>
+        internal static void StandardProcess<T>(IBundleAttribute bundle, bool debug, Func<T, T> d) where T:BundleBase<T>
+        {
+            if(bundle == null) throw  new ArgumentNullException("bundle");
+            //Get the type of T
+            var tType = typeof (T);
+            //declare ds variable (this will be our bundler)
+            dynamic ds = null;
+            //if T is Javascript bundle make our bundler be the javascript bundler
+            if (tType == typeof (SquishIt.Framework.JavaScript.JavaScriptBundle))
+            {
+                ds = Bundle.JavaScript();
+               
+            }
+            //if T is css make our bundler be the javascript bundler
+            else if (tType == typeof (SquishIt.Framework.CSS.CSSBundle))
+            {
+                ds = Bundle.Css();
+            }
+            //if our bundler is still null then we should throw a not supported exception
+            if(ds == null) throw new NotSupportedException();
+            //if our function is not null then we should invoke it
+            if(d != null)ds = d.Invoke((T)ds);
+            //Add a bunch of files
+            foreach (var x in bundle.Fileuri)
+            {
+                ds.Add(x);
+            }
+            //declare a func for our debug flag to use
+            Func<bool> z = () => { return debug; };
+            ds.ForceDebugIf(z);
+            ds.AsCached(bundle.BundleName, bundle.OutputUri);
+        }
+        
+    }
+}
